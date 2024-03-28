3079342dff06e928b035297faea1a112220683ed
Tomasz Ulatowski <Tomek.ulatowski@outlook.com>
2024-03-28 21:29:35 +0100

    added shuffle, standardDeviation and ptr2arr func

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -1,12 +1,13 @@
 #include "arrays.hpp"
 #include <iostream>
+#include <cmath>
 
 
 
-void arrays::print(const int arr_p[], int s) {
-	std::cout << arr_p[0];
+void arrays::print(const int arr[], int s) {
+	std::cout << arr[0];
 	for (int i = 1; i < s; i++)
-		std::cout << "," << arr_p[i];
+		std::cout << "," << arr[i];
 	std::cout << "\n";
 }
 
@@ -52,6 +53,44 @@ int* arrays::deleteRepeats(const int arr[], int& s) {
 }
 
 
+void arrays::swap(int arr[], int id1, int id2) {
+	int temp;
+	temp = arr[id2];
+	arr[id2] = arr[id1];
+	arr[id1] = temp;
+}
+
+
+void arrays::bubbleSort(int arr[], int s) {
+	int temp;
+	int size = s;
+	for (int j = 0; j < size; j++) {
+		for (int i = 0; i < s - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i + 1];
+				arr[i + 1] = arr[i];
+				arr[i] = temp;
+			}
+		}
+		s--;
+	}
+}
+
+
+int* arrays::join(int arr1[], int s1, int arr2[], int s2) {
+	int* arr_p = new int[s1+s2];
+	
+	for (int i = 0; i < s1+s2; i++)
+	{
+		if (i < s1)
+			arr_p[i] = arr1[i];
+		else
+			arr_p[i] = arr2[i-s1];
+	}
+	return arr_p;
+}
+
+
 void arrays::reverse(int arr[], int s) {
 	int temp;
 	for (int i = 0; i < s/2; i++) {
@@ -62,17 +101,45 @@ void arrays::reverse(int arr[], int s) {
 }
 
 
-float arrays::average(const int arr[], int s) {
-	int sum = 0;
+void arrays::shuffle(int arr[], int s) {
+	srand((unsigned)time(0));
+
 	for (int i = 0; i < s; i++) {
-		sum += arr[i];
+		swap(arr, rand() % s, rand() % s);
 	}
-	return sum / static_cast<float>(s);
 }
 
 
-int arrays::median(const int sorted_arr[], int s) {
-	return sorted_arr[s / 2];
+float arrays::mean(const int arr[], int s) {
+	float sum = 0.0f;
+	for (int i = 0; i < s; i++) {
+		sum += arr[i];
+	}
+	return sum / s;
+}
+
+
+float arrays::median(const int sorted_arr[], int s) {
+	if (s % 2 == 0)
+		return static_cast<float>(sorted_arr[s / 2 - 1] + sorted_arr[s / 2]) / 2.0f;
+	else
+		return sorted_arr[s / 2];
+}
+
+
+float arrays::standardDeviation(const int arr[], int s) {
+	float avr = mean(arr, s);
+	int* arr_p = new int[s];
+	for (int i = 0; i < s; i++) {
+		arr_p[i] = abs(arr[i] - avr);
+	}
+	float sum = 0;
+	for (int i = 0; i < s; i++) {
+		sum += pow(arr_p[i], 2);
+	}
+
+	delete[] arr_p;
+	return sqrt(sum / s);
 }
 
 
@@ -113,26 +180,9 @@ bool arrays::compare(const int arr[], int s, const int arr2[], int s2) {
 }
 
 
-void arrays::swap(int arr[], int id1, int id2) {
-	int temp;
-	temp = arr[id2];
-	arr[id2] = arr[id1];
-	arr[id1] = temp;
-}
-
-
-void arrays::bubbleSort(int arr[], int s) {
-	int temp;
-	int size = s;
-	for (int j = 0; j < size; j++) {
-		for (int i = 0; i < s - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				temp = arr[i + 1];
-				arr[i + 1] = arr[i];
-				arr[i] = temp;
-			}
-		}
-		s--;
+void arrays::ptr2arr(int* arr_p, int arr[], int s) {
+	for (int i = 0; i < s; i++) {
+		arr[i] = arr_p[i];
 	}
 }
 
--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 int main() {
 	arrays arrays;
-	int size = 10;
-	int* arr_p = arrays.generate(size);
-	arrays.print(arr_p, size);
-	//int* arr_p2 = arrays.deleteRepeats(arr_p, size);
-	arrays.bubbleSort(arr_p, size);
-	cout << arrays.binarySearch(arr_p, size, 6);
-	arrays.print(arr_p, size);
-	delete[] arr_p;
-	//delete[] arr_p2;
+	//int size = 10;
+	int a[] = { 235,654,34,6,43,23,54 };
+	//int a2[] = { 2,2,1,1,1 };
+	//int arr[8];
+	//int* arr_p = arrays.join(a, 3, a2, 5);
+	arrays.print(a, 7);
+	arrays.shuffle(a, 7);
+	arrays.print(a, 7);
+	//cout << arrays.mean(a, 5);
+
+	//delete[] arr_p;
 }
